cd23725c98b0cf401a5621450a7662324fc91194
mariacav <114124105+mariacav@users.noreply.github.com>
2022-09-26 23:21:59 -0300

    Update index.jsx

--- src/components/Cabeçalho/index.jsx
+++ src/components/Cabeçalho/index.jsx
@@ -2,7 +2,7 @@ export const Cabeçalho = ({props}) => {
     return (
       <header class='topo-pagina'>
 		<div class='logotipo' id="logo">
-			<img class="oi" src="https://www.bocaweb.com.br/images/logobocaweb.png"></img>
+			<img class="oi" src="https://www.bocaweb.com.br/images/logobocaweb.png" alt="logo"></img>
 		</div>
 		<div class='menuprincipal'>
 			<h1>BOCAWEB FRONT</h1>
